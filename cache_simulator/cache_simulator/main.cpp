350e1c6cf83ffa08b468dcaf5d2dba25a27c62de
yechan <yyechan@naver.com>
2020-11-10 20:59:45 +0900

    file read

---
+++ cache_simulator/cache_simulator/main.cpp
@@ -0,0 +1,25 @@
+/* 컴퓨터구조 2015111730 조예찬 */
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+	string filename;
+	cout << "파일이름 입력 : ";
+	cin >> filename;
+
+	ifstream ifs;
+	ifs.open(filename);
+
+	if (!ifs.is_open()) {
+
+		cout << "잘못된 파일이름" << endl;
+		return 0;
+	}
+
+
+}
